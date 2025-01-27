c4f09b4ed113487e7efe33e13e6c80f8596fbda8
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2025-01-27 14:30:12 +0500

    changes made

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ document.querySelector('.start-btn').addEventListener("click", () => {
 
 document.querySelector('.guess-btn').addEventListener("click", () => {
     const input = document.querySelector('.input-number');
-    if (input.value > 100 || input.value < 1) {
+    if (input.value == "") {
+        stats.innerHTML = "Please enter a number...";
+        input.value = ""
+    }
+    else if (input.value > 100 || input.value < 1) {
         stats.innerHTML = "Please enter the number in range <br> The range is 1 to 100";
+        input.value = ""
     } else if (input.value > number) {
         stats.innerHTML = "Try Again! <br> Number you entered is greater than the correct number.";
         attempts = attempts + 1;
+        input.value = ""
     } else if (input.value < number) {
         stats.innerHTML = "Try Again! <br> Number you entered is less than the correct number.";
         attempts = attempts + 1;
+        input.value = ""
     } else if (input.value == number) {
         stats.innerHTML = `Congratulations! <br> You guessed the correct number in ${attempts} attempts <br> <b>Press F5 on your keyboard to play again</b>`;
         attempts = attempts + 1;
+        input.value = ""
     } else {
         stats.innerHTML = "Please enter the right format <br> The format only allows whole numbers";
+        input.value = ""
     }
     stats.style.display = "block";
 });
